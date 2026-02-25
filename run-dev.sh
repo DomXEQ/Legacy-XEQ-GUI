595bc70c40483b73a771d17a5b88a8771397b5aa
DomXEQ <domxeq@proton.me>
2026-02-25 12:50:50 -0800

    build cleanup

--- run-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Bash script to run the GUI wallet in development mode
-# This sets the OpenSSL legacy provider before running npm
-
-export NODE_OPTIONS=--openssl-legacy-provider
-npm run dev
-
